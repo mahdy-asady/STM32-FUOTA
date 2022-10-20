8b421dfe348447e115087b662abfe5c942142fe8
Mahdy Asady <mahdy.asady@gmail.com>
2022-10-20 19:20:33 +0330

    Bootloader app implemented

--- src/app/bootloader/main.c
+++ src/app/bootloader/main.c
@@ -1,18 +1,22 @@
 #include <stdint.h>
 #include "stm32f107xc.h"
+#include "GPIO.h"
 
 int main(void) {
+    GPIO_EnablePort(GPIOD);
+    GPIO_InitPin(GPIOD, PIN_04, PinDirectionOutput);
+    GPIO_TogglePin(GPIOD, PIN_04);
+    for (volatile int i = 0; i < 500000; i++);
+    GPIO_TogglePin(GPIOD, PIN_04);
 
-    //Config GPIO Pin D4 as Output
-    RCC->APB2ENR |= RCC_APB2ENR_IOPDEN;
-    GPIOD->CRL = GPIO_CRL_MODE4_1;
+    //Set Vector Table
+    SCB->VTOR = FLASH_APP1_OFFSET;
 
-    while(1) {
-        GPIOD->ODR |= GPIO_ODR_ODR4;
-        for (volatile int i = 0; i < 500000; i++);
-        GPIOD->ODR &= ~GPIO_ODR_ODR4;
-        for (volatile int i = 0; i < 500000; i++);
-    }
+    //Run Application's ResetHandler
+    void (*app_reset_handler)(void) = (void*)(*((volatile uint32_t*) (FLASH_APP1_OFFSET + 4U)));
+    app_reset_handler();
+
+    while(1);
 }
 
 void SystemInit(void) {
