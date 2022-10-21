805c6d9b45405241b8c96c3b515c7146df1596e4
Mahdy Asady <mahdy.asady@gmail.com>
2022-10-21 14:53:43 +0330

    StackPointer set. some code cleanup added

--- src/app/bootloader/main.c
+++ src/app/bootloader/main.c
@@ -4,26 +4,44 @@
 
 extern char FLASH_APP1_OFFSET;
 
+void BootApplication(void);
+void ShowBootloaderSign(void);
+
 int main(void) {
-    GPIO_EnablePort(GPIOD);
-    GPIO_InitPin(GPIOD, PIN_04, PinDirectionOutput);
-    GPIO_TogglePin(GPIOD, PIN_04);
-    for (volatile int i = 0; i < 500000; i++);
-    GPIO_TogglePin(GPIOD, PIN_04);
 
-    //cleanup. Set anything as untouched
-    GPIO_DisablePort(GPIOD);
+    ShowBootloaderSign();
 
-    //Set Vector Table
-    SCB->VTOR = (uint32_t)&FLASH_APP1_OFFSET;
-
-    //Run Application's ResetHandler
-    void (*app_reset_handler)(void) = (void*)(*((volatile uint32_t*) (&FLASH_APP1_OFFSET + 4U)));
-    app_reset_handler();
+    BootApplication();
 
     while(1);
 }
 
 void SystemInit(void) {
 
+}
+
+void BootApplication(void) {
+    void (*AppResetHandler)(void) = (void*)(*((volatile uint32_t*) (&FLASH_APP1_OFFSET + 4U)));
+
+    //Set Vector Table
+    SCB->VTOR = (uint32_t)&FLASH_APP1_OFFSET;
+    // Set stack pointer
+    __set_MSP((uint32_t)*((__IO uint32_t*)&FLASH_APP1_OFFSET));
+    //Run Application's ResetHandler
+    AppResetHandler();
+}
+
+void ShowBootloaderSign(void) {
+    GPIO_EnablePort(GPIOD);
+    GPIO_InitPin(GPIOD, PIN_04, PinDirectionOutput);
+
+    for(int i = 0; i < 5; i++) {
+        GPIO_TogglePin(GPIOD, PIN_04);
+        for (volatile int j = 0; j < 100000; j++);
+    }
+    for (volatile int j = 0; j < 200000; j++);
+    GPIO_TogglePin(GPIOD, PIN_04);
+
+    //cleanup. Set anything as untouched
+    GPIO_DisablePort(GPIOD);
 }
