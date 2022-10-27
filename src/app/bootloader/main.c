225e02d7a2e29cca8c95d8e1a5585ea8622568f8
Mahdy Asady <mahdy.asady@gmail.com>
2022-10-28 00:04:17 +0330

    Set System Clock to 72MHz

--- src/app/bootloader/main.c
+++ src/app/bootloader/main.c
@@ -16,8 +16,55 @@ int main(void) {
     while(1);
 }
 
+/*
+    Set System Clock to 72Mhz
+*/
 void SystemInit(void) {
+    //Enable HSE & Wait to be ready
+    RCC->CR |= RCC_CR_HSEON;
+    while (!(RCC->CR & RCC_CR_HSERDY));
 
+    //SET PREDIV2 FACTOR TO 5
+    RCC->CFGR2 = (RCC->CFGR2 & ~RCC_CFGR2_PREDIV2) | RCC_CFGR2_PREDIV2_DIV5;
+
+    //SET PLL2MUL FACTOR TO 8
+    RCC->CFGR2 = (RCC->CFGR2 & ~RCC_CFGR2_PLL2MUL) | RCC_CFGR2_PLL2MUL8;
+
+    //ENABLE PLL2
+    RCC->CR |= RCC_CR_PLL2ON;
+    while (!(RCC->CR & RCC_CR_PLL2RDY));
+
+    //SET PREDIV1 SRC TO PLLMULL2
+    RCC->CFGR2 |= RCC_CFGR2_PREDIV1SRC_PLL2;
+
+    //SET PREDIV1 FACTOR TO 5
+    RCC->CFGR2 = (RCC->CFGR2 & ~RCC_CFGR2_PREDIV1) | RCC_CFGR2_PREDIV1_DIV5;
+
+    //SET PLL CLOCK SOURCE TO PREDIV1
+    RCC->CFGR |= RCC_CFGR_PLLSRC;
+
+    //Set PLLMul to 9
+    RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PLLMULL) | RCC_CFGR_PLLMULL9;
+
+    //Enable PLL & Wait to be ready
+    RCC->CR |= RCC_CR_PLLON;
+    while (!(RCC->CR & RCC_CR_PLLRDY));
+
+    //Enable flash Prefetch buffer & Set Flash latency to 2
+    FLASH->ACR |= FLASH_ACR_PRFTBE | FLASH_ACR_LATENCY_2;
+
+    //AHB NO Divider
+    RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_HPRE) | RCC_CFGR_HPRE_DIV1;
+
+    //SET SW TO PLL
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+    while (!(RCC->CFGR & (RCC_CFGR_SWS_PLL)));
+
+    //APB1 Divide by 2
+    RCC->CFGR = (RCC->CFGR & ~RCC_CFGR_PPRE1) | RCC_CFGR_PPRE1_DIV2;
+
+    //APB2 NO Divider
+    RCC->CFGR &= ~RCC_CFGR_PPRE2;
 }
 
 void BootApplication(void) {
@@ -37,9 +84,9 @@ void ShowBootloaderSign(void) {
 
     for(int i = 0; i < 5; i++) {
         GPIO_TogglePin(GPIOD, PIN_04);
-        for (volatile int j = 0; j < 100000; j++);
+        for (volatile int j = 0; j < 300000; j++);
     }
-    for (volatile int j = 0; j < 200000; j++);
+    for (volatile int j = 0; j < 600000; j++);
     GPIO_TogglePin(GPIOD, PIN_04);
 
     //cleanup. Set anything as untouched
