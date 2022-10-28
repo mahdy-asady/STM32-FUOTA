acc131754a947b35a2e1d49640c2d9d4e3a0309e
Mahdy Asady <mahdy.asady@gmail.com>
2022-10-28 13:29:41 +0330

    USART2 implemented

--- src/mcu/stm32f107/USART.c
+++ src/mcu/stm32f107/USART.c
@@ -2,6 +2,7 @@
 #include "GPIO.h"
 
 void (*USART1CallBack)(char);
+void (*USART2CallBack)(char);
 
 void USART_EnableUSART1(void) {
     RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
@@ -22,6 +23,27 @@ void USART_FetchUSART1(void (*CallBack)(char)) {
 }
 
 
+void USART_EnableUSART2(void) {
+    RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+    RCC->APB1ENR |= RCC_APB1ENR_USART2EN;
+
+    AFIO->MAPR |= AFIO_MAPR_USART2_REMAP;
+
+    GPIO_EnablePort(GPIOD);
+    GPIO_InitPin(GPIOD, PIN_05, PinOperationOutputAlternatePushPull);
+    GPIO_InitPin(GPIOD, PIN_06, PinOperationInputPullUpDown);
+    
+    USART2->BRR = 20 << 4; // Baud rate = 36Mhz/(16 * __20__) = 112500
+    USART2->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
+}
+
+void USART_FetchUSART2(void (*CallBack)(char)) {
+    USART2CallBack = CallBack;
+    __NVIC_EnableIRQ(USART2_IRQn);
+    USART2->CR1 |= USART_CR1_RXNEIE;
+}
+
+
 void USART_SendString(USART_TypeDef *USART, char *Text) {
     while(*Text != 0) {
         while(!(USART->SR & USART_SR_TC));
@@ -34,4 +56,10 @@ void USART_SendString(USART_TypeDef *USART, char *Text) {
 void USART1_IRQHandler (void) {
     char Data = (char)((uint8_t)(USART1->DR) & 0x7F);
     USART1CallBack(Data);
+}
+
+
+void USART2_IRQHandler (void) {
+    char Data = (char)((uint8_t)(USART2->DR) & 0x7F);
+    USART2CallBack(Data);
 }
--- src/peripheral/USART.h
+++ src/peripheral/USART.h
@@ -3,9 +3,14 @@
 
 #include "stm32f107xc.h"
 
-void USART_EnableUSART1(void);
 
+//USART 1
+void USART_EnableUSART1(void);
 void USART_FetchUSART1(void (*CallBack)(char));
+
+//USART 2
+void USART_EnableUSART2(void);
+void USART_FetchUSART2(void (*CallBack)(char));
 
 
 void USART_SendString(USART_TypeDef*, char*);
