34b2ff603293605d208cbb5cf1eb52e59f304ae9
Mahdy Asady <mahdy.asady@gmail.com>
2022-11-01 01:49:23 +0330

    delay function splitted to 2 sub function

--- src/shared/delay.c
+++ src/shared/delay.c
@@ -1,11 +1,21 @@
 #include <stdint.h>
 #include "stm32f107xc.h"
 
-
-void _delay_ms(uint16_t duration) {
-    uint32_t Counter = duration * (F_CPU/8)/1000;
+void DelaySet(uint16_t Duration) {
+    uint32_t Counter = Duration * (F_CPU/8)/1000;
     SysTick->LOAD = Counter & 0xFFFFFF;
     SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;
-    while(!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
-    SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
+    SysTick->CTRL &= ~SysTick_CTRL_COUNTFLAG_Msk;
+}
+
+int DelayCatched(void) {
+    int Result = SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk;
+    if(Result)
+        SysTick->CTRL &= ~SysTick_CTRL_ENABLE_Msk;
+    return Result;
+}
+
+void _delay_ms(uint16_t Duration) {
+    DelaySet(Duration);
+    while(!DelayCatched());
 }
--- src/shared/delay.h
+++ src/shared/delay.h
@@ -1,6 +1,9 @@
 #ifndef _DELAY_H_
 #define _DELAY_H_
 
+void DelaySet(uint16_t Duration);
+int DelayCatched(void);
+
 void _delay_ms(uint16_t duration);
 
 #endif
