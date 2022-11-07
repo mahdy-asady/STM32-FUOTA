7603e0df4b7437c63a32e173fd57c4a3eab5396b
Mahdy Asady <mahdy.asady@gmail.com>
2022-11-08 00:05:46 +0330

    Unneccessary variable removed

--- src/mcu/stm32f107/USART.c
+++ src/mcu/stm32f107/USART.c
@@ -138,7 +138,7 @@ void ReceiveData(USART_Handle *USART) {
         if(NewEnd == USART->Buffer.Start) {
             //Buffer overflow
             //we will miss some characters on buffer overflow
-            char TempChar = (char)((uint8_t)(USART->Instance->DR) & 0x7F);
+            (void)(USART->Instance->DR);
             return;
         }
         char Char = (char)((uint8_t)(USART->Instance->DR) & 0x7F);
