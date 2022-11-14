ee8f067f5fc67ebcdced631c24a0b349044309a2
Mahdy Asady <mahdy.asady@gmail.com>
2022-11-14 03:32:48 +0330

    Update application to use new headers

--- src/app/application/main.c
+++ src/app/application/main.c
@@ -1,23 +1,39 @@
 #include "GPIO.h"
 #include "USART.h"
+#include "debug.h"
+#include "timer.h"
+
+
+USART_Handle UsartDebug;
+
+void InitSystem(void);
+
 
 int main(void)
 {
-    GPIO_EnablePort(GPIOD);
-    GPIO_InitPin(GPIOD, PIN_02, PinOperationOutputPushPull);
+    InitSystem();
 
-    USART_EnableUSART1();
-    
-    char *Text = "Application Started!!\r\n";
-    USART_SendString(USART1, Text);
+    log_info(&UsartDebug, "Application Started!!");
 
     while (1)
     {
         GPIO_TogglePin(GPIOD, PIN_02);
-        for (volatile int i = 0; i < 500000; i++);
+        Delay_ms(500);
     }
 }
 
-void SystemInit(void) {
+void SystemInit(void) {}
 
+void UsartInit(void) {
+    UsartDebug.Instance = USART1;
+    UsartDebug.BaudRate = 40 << 4;
+    UsartDebug.isPortMapped = 0;
+    USART_Init(&UsartDebug);
+}
+
+void InitSystem(void) {
+    GPIO_EnablePort(GPIOD);
+    GPIO_InitPin(GPIOD, PIN_02, PinOperationOutputPushPull);
+    UsartInit();
+    TimerInit();
 }
