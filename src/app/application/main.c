e5e765c7b747f1b022b76d4b10110c90eac850f6
Mahdy Asady <mahdy.asady@gmail.com>
2022-11-14 04:21:05 +0330

    Now application will reboot to backup on button press

--- src/app/application/main.c
+++ src/app/application/main.c
@@ -2,6 +2,8 @@
 #include "USART.h"
 #include "debug.h"
 #include "timer.h"
+#include "bootOptions.h"
+#include "bkpreg.h"
 
 
 USART_Handle UsartDebug;
@@ -31,9 +33,37 @@ void UsartInit(void) {
     USART_Init(&UsartDebug);
 }
 
+void ButtonInterruptEnable(void) {
+    //Enable Interrupt on PA0
+    GPIO_EnablePort(GPIOA);
+    GPIO_InitPin(GPIOA, PIN_00, PinOperationInputPullUpDown);
+    //GPIO_WritePin(GPIOA, PIN_00, PinLow);
+
+    RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+    AFIO->EXTICR[1] = (AFIO->EXTICR[1] & ~AFIO_EXTICR2_EXTI4) | AFIO_EXTICR2_EXTI4_PA;
+    EXTI->IMR |= EXTI_IMR_IM0;
+    EXTI->RTSR |= EXTI_RTSR_RT0;
+    EXTI->FTSR &= ~EXTI_FTSR_FT0;
+    NVIC_SetPriority(EXTI0_IRQn, 1);
+    NVIC_EnableIRQ(EXTI0_IRQn);
+}
+
 void InitSystem(void) {
     GPIO_EnablePort(GPIOD);
     GPIO_InitPin(GPIOD, PIN_02, PinOperationOutputPushPull);
+
     UsartInit();
     TimerInit();
+    BackupRegInit();
+
+    ButtonInterruptEnable();
+}
+
+void EXTI0_IRQHandler(void) {
+    if(EXTI->PR & EXTI_PR_PIF0) {
+        log_info(&UsartDebug, "System set to update!!");
+        BackupRegWrite(0, BOOT_UPDATE);
+        NVIC_SystemReset();
+        EXTI->PR |= EXTI_PR_PIF0;
+    }
 }
