fc7b38dd32c6cddce14e87b7b944ec1a35c0f061
Mahdy Asady <mahdy.asady@gmail.com>
2022-11-15 13:55:04 +0330

    Boot options definistions converted to enum

--- src/shared/bootOptions.h
+++ src/shared/bootOptions.h
@@ -1,12 +1,11 @@
 #ifndef _BOOTOPTIONS_H_
 #define _BOOTOPTIONS_H_
 
-#define BOOT_NORMAL     0
-
-#define BOOT_UPDATE     1
-
-#define BOOT_BACKUP     2
-
-#define BOOT_RESTORE    3
+enum bootOptions {
+    BOOT_NORMAL,
+    BOOT_UPDATE,
+    BOOT_BACKUP,
+    BOOT_RESTORE
+};
 
 #endif
